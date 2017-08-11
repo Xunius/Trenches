43cfcf4e6f14b1c1ba741c3440d657a313ecccc4
Xunius <xugzhi1987@gmail.com>
2017-08-11 17:38:51 +0800

    Add files via upload

---
+++ cus_font/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+PATH=os.path.dirname(os.path.realpath(__file__))
+
+E_FONT_FILE='TempoFont.ttf'
+E_FONT_FILE8='slkscr.ttf'
+
+E_FONT_PATH=os.path.join(PATH,E_FONT_FILE)
+E_FONT_PATH8=os.path.join(PATH,E_FONT_FILE8)
+
+
